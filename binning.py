7a5142f3ddd187ac5793860e94f150a604b7c741
Jessica Xue <jessicaxu@college.harvard.edu>
2015-01-21 16:14:47 -0500

    sqrt sadness

--- binning.py
+++ binning.py
@@ -7,33 +7,39 @@
 import csv
 import sys
 
-bins = {}
-fwrite = open(sys.argv[3], 'w')
+def sadness(a):
+    a + 1
 
-with open(sys.argv[1], 'rb') as csvfile:
-    with open(sys.argv[2], 'rb') as csvfile2:
-        spamreader = csv.DictReader(csvfile)
-        spamreader2 = csv.DictReader(csvfile2)
-        spam1 = []
-        spam2 = []
-        for row in spamreader:
-            spam1.append(row)
 
-        for row in spamreader2:
-            spam2.append(row)
+def bin(train, test, out):
+    bins = {}
+    fwrite = open(out, 'w')
 
-        for j in range(len(spam1)):
-            row = spam1[j]
-            row2 = spam2[j]
+    with open(train, 'rb') as csvfile:
+        with open(test, 'rb') as csvfile2:
+            spamreader = csv.DictReader(csvfile)
+            spamreader2 = csv.DictReader(csvfile2)
+            spam1 = []
+            spam2 = []
+            for row in spamreader:
+                spam1.append(row)
 
-            for key in row:
-                bins[row[key]] = row2[key]
+            for row in spamreader2:
+                spam2.append(row)
 
-        for x in range(len(spam1)):
-            row = spam1[x]
-            for key in row:
-                row[key] = bins[row[key]]
+            for j in range(len(spam1)):
+                row = spam1[j]
+                row2 = spam2[j]
 
-        for y in range(len(spam1)):
-            fwrite.write(",".join(spam1[y].values()) + "\n")
+                for key in row:
+                    bins[row[key]] = row2[key]
+
+            for x in range(len(spam1)):
+                row = spam1[x]
+                for key in row:
+                    row[key] = bins[row[key]]
+
+            for y in range(len(spam1)):
+                fwrite.write(",".join(spam1[y].values()) + "\n")
+            return  spam1
 
