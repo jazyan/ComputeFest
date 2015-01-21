3dd0ee86d7def99c3a85084fe46f5c1686b2bc53
jasmineyan <jasmineyan@college.harvard.edu>
2015-01-20 21:13:56 -0500

    stuff

--- badthings.py
+++ badthings.py
@@ -6,27 +6,39 @@ age = []
 race = []
 maritalstatus= []
 education = []
-nativecountry = [] 
+nativecountry = []
 workclass = []
 occupation = []
 salaryclass = []
 
 i = 0
+with open('CC_test1.csv', 'rb') as csvreader:
+    with open('CC_test_orig.csv', 'rb') as csvreader2:
+        spamreader = csv.DictReader(csvreader, delimiter=';')
+        spamreader2 = csv.DictReader(csvreader2, delimiter=';')
+        spam1 = []
+        spam2 = []
+        for row in spamreader:
+            spam1.append(row)
+            i += 1
+            if (i == 800):
+                break;
+        i = 0
+        for row in spamreader2:
+            spam2.append(row)
+            i += 1
+            if (i == 800):
+                break;
 
-with open('CC_test_orig.csv', 'rb') as csvfile:
-    spamreader = csv.DictReader(csvfile, delimiter=';')
-    for row in spamreader:
-        sex.insert(i, row['sex'])
-        age.insert(i, row['age'])
-        maritalstatus.insert(i, row['marital-status'])
-        education.insert(i, row['education'])
-        nativecountry.insert(i, row['native-country'])
-        workclass.insert(i,row['workclass'])
-        occupation.insert(i, row['occupation'])
-        salaryclass.insert(i, row['salary-class'])
-
-
-        i += 1
+for j in range(800):
+    sex.append(spam1[j]['sex'] + spam2[j]['sex'])
+    age.append(spam1[j]['age'] + spam2[j]['age'])
+    maritalstatus.append(spam1[j]['marital-status'] + spam2[j]['marital-status'])
+    education.append(spam1[j]['education']+spam2[j]['education'])
+    nativecountry.append(spam1[j]['native-country'] + spam2[j]['native-country'])
+    workclass.append(spam1[j]['workclass'] + spam2[j]['workclass'])
+    occupation.append(spam1[j]['occupation'] + spam2[j]['occupation'])
+    salaryclass.append(spam1[j]['salary-class'] + spam2[j]['salary-class'])
 
 print set(age)
 print set(maritalstatus)
